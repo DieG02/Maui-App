697f3eede0d58c13f1d5c4d0ce782c49a973fa67
DieG02 <43993554@alejandrovolta.edu.ar>
2022-08-15 22:18:36 -0300

    delete commits

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -5,20 +5,6 @@ export const separator = (num: string | number): string => {
   return num.length > 0 ? (num + '').replace(reg, '$&.') : '';
 }
 
-// export const round = (num: string): string => {
-//   const i = num.indexOf(",");
-//   const [integer, decimal] = num.split(",");
-//   num = integer + (decimal && parseInt(decimal) > 0 ? "," + decimal.replace(/[\s\.]/g, '') : '');
-//   if(i === -1) return num; // Without ","
-//   else if (i === num.length - 1) return num.slice(0, -1); // Ends like "123,"
-//   // else return num.slice(0, i + 3); // Round to 2 decimals after ","
-//   else {
-//     const float = parseFloat(0 + "." + decimal);
-//     const rounded = Math.round((float + Number.EPSILON) * 100);
-//     console.log({ rounded });
-//     return integer + (rounded ? "," + rounded : ""); // Round to 2 decimals after ","
-//   }
-// }
 
 export const round = (num: string): string => { // 4.278,358
   num = num.replace(/[\s\.]/g, ''); // 4278,358
