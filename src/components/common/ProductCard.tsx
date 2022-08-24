8322ca11493787df826a454807ae7b26888d0362
Billy Bautista <bbautista@litebox.ai>
2022-08-24 19:16:34 -0300

    fix: 🐛 fixed card to use in InventoryScreen

--- src/components/common/ProductCard.tsx
+++ src/components/common/ProductCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Text, View, Image, TouchableOpacity } from "react-native";
-import { getAllProductsResponseDto } from "../../../../Maui-Backend/src/controllers/types";
+import { getAllItemsResponseDto } from "../../../../Maui-Backend/src/controllers/types";
 import globalStyles from "../../styles/globalStyles";
 import Icon from "react-native-vector-icons/FontAwesome5";
 
@@ -8,7 +8,7 @@ const { secondaryColor, mainColor } = globalStyles;
 
 interface Props {
   onPress?: () => void;
-  data: getAllProductsResponseDto[0];
+  data: getAllItemsResponseDto[0];
 }
 
 const ProductCard = ({ onPress, data }: Props) => {
@@ -18,7 +18,7 @@ const ProductCard = ({ onPress, data }: Props) => {
       style={{
         backgroundColor: "#F9F9F9",
         borderRadius: 15,
-        marginTop: 20
+        marginTop: 20,
       }}
     >
       <View
@@ -26,60 +26,65 @@ const ProductCard = ({ onPress, data }: Props) => {
           flexDirection: "row",
           alignItems: "center",
           borderRadius: 40,
-          marginHorizontal: 20
+          marginHorizontal: 20,
         }}
       >
         <View
           style={{
             marginVertical: 20,
             width: "70%",
-            marginRight: 20
+            marginRight: 20,
           }}
         >
           <Text
             style={{
               fontSize: 16,
               fontFamily: "Gilroy-SemiBold",
-              color: secondaryColor
+              color: secondaryColor,
             }}
           >
             {data.name}
           </Text>
-          <Text
-            style={{
-              color: mainColor,
-              fontSize: 16,
-              fontFamily: "Gilroy-SemiBold",
-              marginVertical: 5
-            }}
-          >
-            Stock: {data.stock}
-          </Text>
-          {/* <View
-            style={{
-              backgroundColor: "#FF8000",
-              borderRadius: 15,
-              paddingHorizontal: 10,
-              width: 90,
-              marginVertical: 5,
-            }}
-          >
+          {"stock" in data ? (
             <Text
               style={{
-                color: "white",
-                fontSize: 14,
+                color: mainColor,
+                fontSize: 16,
                 fontFamily: "Gilroy-SemiBold",
                 marginVertical: 5,
               }}
             >
-              Servicio
+              Stock: {data.stock}
             </Text>
-          </View> */}
+          ) : (
+            <View
+              style={{
+                backgroundColor: "#FF8000",
+                borderRadius: 15,
+                paddingHorizontal: 10,
+                width: 90,
+                marginVertical: 5,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Text
+                style={{
+                  color: "white",
+                  fontSize: 14,
+                  fontFamily: "Gilroy-SemiBold",
+                  marginVertical: 5,
+                }}
+              >
+                Servicio
+              </Text>
+            </View>
+          )}
           <Text
             style={{
               fontSize: 18,
               fontFamily: "Gilroy-SemiBold",
-              color: secondaryColor
+              color: secondaryColor,
             }}
           >
             $ {data.retailPrice}
@@ -89,7 +94,7 @@ const ProductCard = ({ onPress, data }: Props) => {
           <Image
             resizeMode="contain"
             source={{
-              uri: data.image
+              uri: data.image,
             }}
             style={{ width: 70, height: 70 }}
           />
@@ -101,7 +106,7 @@ const ProductCard = ({ onPress, data }: Props) => {
               backgroundColor: "#60708F",
               borderRadius: 10,
               alignItems: "center",
-              justifyContent: "center"
+              justifyContent: "center",
             }}
           >
             <Icon name="file-image" size={40} color="white" />
