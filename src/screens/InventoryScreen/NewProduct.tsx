ddedfaec77568b1573753e5d72a390bbee663537
Billy Bautista <bbautista@litebox.ai>
2022-10-05 22:26:41 -0300

    refactor: 💡 deleted notifications and budget

--- src/screens/InventoryScreen/NewProduct.tsx
+++ src/screens/InventoryScreen/NewProduct.tsx
@@ -6,11 +6,7 @@ import CommonInput from "../../components/common/CommonInput";
 import OptionModal from "../../components/common/OptionModal";
 import { useMutation, useQuery } from "react-query";
 import { createNewProduct } from "../../services/products";
-import { createNewService } from "../../services/services";
-import {
-  createNewProductBodyInputDto,
-  createServiceBodyInputDto,
-} from "../../../../Maui-Backend/src/controllers/types";
+import { createNewProductBodyInputDto } from "../../../../Maui-Backend/src/controllers/types";
 import globalStyles from "../../styles/globalStyles";
 import { getItemCategories } from "../../services/itemCategories";
 import Button from "../../components/common/Button";
@@ -20,12 +16,10 @@ import { BackHeaderTitle } from "../../components/common/HeaderTitle";
 
 const { width } = Dimensions.get("window");
 
-const { secondaryColor, item } = globalStyles;
+const { item, textBlack } = globalStyles;
 interface Props {
   navigation: NavigationProp<any, any>;
 }
-
-const ITEM = ["Producto", "Servicio"];
 
 const UNITS = [
   { name: "Unidad", value: "UNIT" },
@@ -44,11 +38,9 @@ const NewIncome = ({ navigation }: Props) => {
   const [stock, setStock] = useState("");
   const [description, setDescription] = useState("");
   const [costUnit, setCostUnit] = useState("");
-  const [isProduct, setIsProduct] = useState(ITEM[0]);
   const [category, setCategory] = useState("Seleccione una categoría");
   const [quantity, setQuantity] = useState("");
   const [unit, setUnit] = useState("Ingrese el valor");
-  const [modalState, setModalState] = useState(false);
 
   const [modalExpenseCategory, setModalExpenseCategory] = useState(false);
   const [modalUnit, setModalUnit] = useState(false);
@@ -71,20 +63,10 @@ const NewIncome = ({ navigation }: Props) => {
     unit: unit,
   };
 
-  const service: createServiceBodyInputDto = {
-    name: name,
-    retailPrice: +price,
-    categoryId: itemCategories && handleIdCategory(category, itemCategories),
-    description: description,
-  };
-
   const { mutateAsync: createProduct } = useMutation(createNewProduct);
-  const { mutateAsync: createService } = useMutation(createNewService);
 
   const handleSubmit = () => {
-    if (isProduct === ITEM[0]) {
-      createProduct(product);
-    } else createService(service);
+    createProduct(product);
   };
 
   return (
@@ -107,7 +89,7 @@ const NewIncome = ({ navigation }: Props) => {
           <Text
             style={{
               fontSize: 18,
-              color: secondaryColor,
+              color: textBlack,
               fontFamily: "Gilroy-Bold",
               marginBottom: 10,
             }}
@@ -128,135 +110,26 @@ const NewIncome = ({ navigation }: Props) => {
               <Text>Imagen</Text>
             </View>
           </View>
-          <OptionModal
-            title="Tipo de Item"
-            options={ITEM}
-            isModalVisible={modalState}
-            setIsModalVisible={setModalState}
-            selectedOption={isProduct}
-            setSelectedOption={setIsProduct}
+          <CommonInput
+            placeholder="¿Como quieres llamar a este producto?"
+            name="Nombre del Producto"
+            marginBottom={25}
+            value={name}
+            setValue={setName}
           />
-          {isProduct === "Producto" ? (
-            <>
-              <CommonInput
-                placeholder="¿Como quieres llamar a este producto?"
-                name="Nombre del Producto"
-                marginBottom={25}
-                value={name}
-                setValue={setName}
-              />
-              <View
-                style={{
-                  display: "flex",
-                  flexDirection: "row",
-                  justifyContent: "space-between",
-                }}
-              >
-                <View
-                  style={{
-                    display: "flex",
-                    width: (width - 100) / 2,
-                  }}
-                >
-                  <InputForm
-                    keyboardType="numeric"
-                    placeholder="0,00"
-                    value={price}
-                    name="Valor"
-                    setValue={setPrice}
-                    marginBottom={25}
-                  />
-                </View>
-                <View
-                  style={{
-                    display: "flex",
-                    width: (width - 100) / 2,
-                  }}
-                >
-                  <CommonInput
-                    placeholder="Ingrese el valor"
-                    name="Stock"
-                    marginBottom={25}
-                    value={stock}
-                    setValue={setStock}
-                    keyboardType="numeric"
-                  />
-                </View>
-              </View>
-              <CommonInput
-                placeholder="Desciripción Opcional"
-                name="Descripción"
-                marginBottom={25}
-                value={description}
-                setValue={setDescription}
-              />
-              <OptionModal
-                title="Categoría"
-                options={
-                  itemCategories?.map((category) => category?.name) ?? []
-                }
-                isModalVisible={modalExpenseCategory}
-                setIsModalVisible={setModalExpenseCategory}
-                selectedOption={category}
-                setSelectedOption={setCategory}
-              />
-              <InputForm
-                keyboardType="numeric"
-                placeholder="0,00"
-                value={costUnit}
-                name="Costo Unitario"
-                setValue={setCostUnit}
-                marginBottom={25}
-              />
-              <View
-                style={{
-                  display: "flex",
-                  flexDirection: "row",
-                  justifyContent: "space-between",
-                }}
-              >
-                <View
-                  style={{
-                    display: "flex",
-                    width: (width - 100) / 2,
-                  }}
-                >
-                  <OptionModal
-                    title="Contenido"
-                    options={UNITS?.map((category) => category?.name) ?? []}
-                    isModalVisible={modalUnit}
-                    setIsModalVisible={setModalUnit}
-                    selectedOption={unit}
-                    setSelectedOption={setUnit}
-                  />
-                </View>
-
-                <View
-                  style={{
-                    display: "flex",
-                    width: (width - 100) / 2,
-                  }}
-                >
-                  <CommonInput
-                    placeholder="Ingrese el valor"
-                    name="Cantidad"
-                    marginBottom={25}
-                    value={quantity}
-                    setValue={setQuantity}
-                    keyboardType="numeric"
-                  />
-                </View>
-              </View>
-            </>
-          ) : (
-            <>
-              <CommonInput
-                placeholder="¿Como quieres llamar a este servicio?"
-                name="Nombre del Servicio"
-                marginBottom={25}
-                value={name}
-                setValue={setName}
-              />
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
+            <View
+              style={{
+                display: "flex",
+                width: (width - 100) / 2,
+              }}
+            >
               <InputForm
                 keyboardType="numeric"
                 placeholder="0,00"
@@ -265,25 +138,85 @@ const NewIncome = ({ navigation }: Props) => {
                 setValue={setPrice}
                 marginBottom={25}
               />
+            </View>
+            <View
+              style={{
+                display: "flex",
+                width: (width - 100) / 2,
+              }}
+            >
               <CommonInput
-                placeholder="Desciripción Opcional"
-                name="Descripción"
+                placeholder="Ingrese el valor"
+                name="Stock"
                 marginBottom={25}
-                value={description}
-                setValue={setDescription}
+                value={stock}
+                setValue={setStock}
+                keyboardType="numeric"
               />
+            </View>
+          </View>
+          <CommonInput
+            placeholder="Desciripción Opcional"
+            name="Descripción"
+            marginBottom={25}
+            value={description}
+            setValue={setDescription}
+          />
+          <OptionModal
+            title="Categoría"
+            options={itemCategories?.map((category) => category?.name) ?? []}
+            isModalVisible={modalExpenseCategory}
+            setIsModalVisible={setModalExpenseCategory}
+            selectedOption={category}
+            setSelectedOption={setCategory}
+          />
+          <InputForm
+            keyboardType="numeric"
+            placeholder="0,00"
+            value={costUnit}
+            name="Costo Unitario"
+            setValue={setCostUnit}
+            marginBottom={25}
+          />
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
+            <View
+              style={{
+                display: "flex",
+                width: (width - 100) / 2,
+              }}
+            >
               <OptionModal
-                title="Categoría"
-                options={
-                  itemCategories?.map((category) => category?.name) ?? []
-                }
-                isModalVisible={modalExpenseCategory}
-                setIsModalVisible={setModalExpenseCategory}
-                selectedOption={category}
-                setSelectedOption={setCategory}
+                title="Contenido"
+                options={UNITS?.map((category) => category?.name) ?? []}
+                isModalVisible={modalUnit}
+                setIsModalVisible={setModalUnit}
+                selectedOption={unit}
+                setSelectedOption={setUnit}
               />
-            </>
-          )}
+            </View>
+
+            <View
+              style={{
+                display: "flex",
+                width: (width - 100) / 2,
+              }}
+            >
+              <CommonInput
+                placeholder="Ingrese el valor"
+                name="Cantidad"
+                marginBottom={25}
+                value={quantity}
+                setValue={setQuantity}
+                keyboardType="numeric"
+              />
+            </View>
+          </View>
         </View>
       </ScrollContainer>
       <View
