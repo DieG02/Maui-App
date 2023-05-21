c8ceba214421e470ac9615ba43304f530e60fb15
Billy Bautista <bbautista@litebox.ai>
2023-05-21 20:24:45 -0300

    changed colors in ContactCard and DebtContact

--- src/v0.1/components/common/DebtContactCard.tsx
+++ src/v0.1/components/common/DebtContactCard.tsx
@@ -4,38 +4,46 @@ import Icon from "react-native-vector-icons/FontAwesome";
 import customStyles from "../../styles/customStyles";
 import Right from "react-native-vector-icons/Entypo";
 import moment from "moment";
-import 'moment/locale/es';
+import "moment/locale/es";
 
-const { mainColor, textBlack, background, secondaryColor, expense, income } = customStyles;
+const { textBlack, background, secondaryColor, expense, income, royalBlue } =
+  customStyles;
 
 interface Props {
   type: string;
   onPress: () => void;
-  date: string
-  sales: number
-  purchases?: string
-  totalPrice: number
-  name: string
+  date: string;
+  sales: number;
+  purchases?: string;
+  totalPrice: number;
+  name: string;
 }
 
-const DebtContactCard = ({ date, type, onPress, sales, purchases, totalPrice, name }: Props) => {
-
+const DebtContactCard = ({
+  date,
+  type,
+  onPress,
+  sales,
+  purchases,
+  totalPrice,
+  name,
+}: Props) => {
   moment.locale("es");
-  const formattedDate = moment(date).format('D [de] MMMM');
+  const formattedDate = moment(date).format("D [de] MMMM");
 
   const renderTypeContact = () => {
     switch (type) {
       case "client": {
         return (
           <View style={styles.iconType}>
-            <Icon name="user" size={25} color={mainColor} />
+            <Icon name="user" size={25} color={royalBlue} />
           </View>
         );
       }
       case "provider": {
         return (
           <View style={styles.iconType}>
-            <Icon name="truck" size={25} color={mainColor} />
+            <Icon name="truck" size={25} color={royalBlue} />
           </View>
         );
       }
@@ -45,11 +53,7 @@ const DebtContactCard = ({ date, type, onPress, sales, purchases, totalPrice, na
   const renderTypeDescription = () => {
     switch (type) {
       case "client": {
-        return (
-          <Text style={styles.descriptionType}>
-            Ventas: {sales}
-          </Text>
-        );
+        return <Text style={styles.descriptionType}>Ventas: {sales}</Text>;
       }
       case "provider": {
         return (
@@ -93,8 +97,8 @@ const DebtContactCard = ({ date, type, onPress, sales, purchases, totalPrice, na
       width: 50,
       height: 50,
       backgroundColor: secondaryColor,
-      borderRadius: 15,
-      marginRight: 30,
+      borderRadius: 25,
+      marginRight: 15,
       alignItems: "center",
       justifyContent: "center",
     },
@@ -109,30 +113,24 @@ const DebtContactCard = ({ date, type, onPress, sales, purchases, totalPrice, na
     },
     row: {
       flexDirection: "row",
-    }
+      alignItems: "center",
+    },
   });
   return (
-    <TouchableOpacity
-      style={styles.container}
-      onPress={onPress}
-    >
+    <TouchableOpacity style={styles.container} onPress={onPress}>
       <View style={styles.row}>
         {renderTypeContact()}
         <View style={{ alignSelf: "center" }}>
-          <Text style={[styles.label, { color: textBlack }]}>
-            {name}
-          </Text>
+          <Text style={[styles.label, { color: textBlack }]}>{name}</Text>
           {renderTypeDescription()}
         </View>
       </View>
       <View style={styles.row}>
         <View style={{ alignItems: "flex-end" }}>
           {renderTypePrice()}
-          <Text style={styles.descriptionType}>
-            {formattedDate}
-          </Text>
+          <Text style={styles.descriptionType}>{formattedDate}</Text>
         </View>
-        <Right name="chevron-small-right" color={textBlack} size={35} />
+        <Right name="chevron-small-right" color={textBlack} size={30} />
       </View>
     </TouchableOpacity>
   );
