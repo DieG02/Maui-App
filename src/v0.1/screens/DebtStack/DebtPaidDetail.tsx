c2ef7b98b5d1a1bf6f15ac964d3b1098718dfff1
Billy Bautista <bbautista@litebox.ai>
2023-07-20 14:57:59 -0300

    add styles and prettier

--- src/v0.1/screens/DebtStack/DebtPaidDetail.tsx
+++ src/v0.1/screens/DebtStack/DebtPaidDetail.tsx
@@ -1,91 +1,99 @@
-import { View, Text, StyleSheet } from 'react-native'
-import customStyles from '../../styles/customStyles'
-import { useMemo } from 'react'
+import { View, Text, StyleSheet } from 'react-native';
+import customStyles from '../../styles/customStyles';
+import { useMemo } from 'react';
 
 interface Props {
-    amountPaid?: number
-    totalAmount?: number
+  amountPaid?: number;
+  totalAmount?: number;
 }
 
 const { expense, secondaryColor, orange, textBlack } = customStyles;
 
 const styles = StyleSheet.create({
-    cardContainer: {
-        backgroundColor: secondaryColor,
-        height: 100,
-        borderRadius: 10,
-        justifyContent: "space-around",
-        alignItems: "center",
-        marginHorizontal: 10,
-        padding: 15,
-        marginBottom: 20,
-    },
-    cardLabel: {
-        justifyContent: "space-between",
-        flexDirection: "row",
-        width: "100%",
-    },
-    progressBarBase: {
-        width: "100%",
-        backgroundColor: "#EAEAEA",
-        borderRadius: 12,
-        height: 18,
-    },
-    processBarLabel: {
-        paddingRight: 10,
-        textAlign: "right",
-        fontSize: 10,
-        fontWeight: "bold",
-        color: "white",
-    }
-})
+  cardContainer: {
+    backgroundColor: secondaryColor,
+    height: 100,
+    borderRadius: 10,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    marginHorizontal: 10,
+    padding: 15,
+    marginBottom: 20,
+  },
+  cardLabel: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    width: '100%',
+  },
+  progressBarBase: {
+    width: '100%',
+    backgroundColor: '#EAEAEA',
+    borderRadius: 12,
+    height: 18,
+  },
+  processBarLabel: {
+    paddingRight: 10,
+    textAlign: 'right',
+    fontSize: 10,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
 
-const paidStyles = (paidValue: number) => StyleSheet.create({
+const paidStyles = (paidValue: number) =>
+  StyleSheet.create({
     progressBar: {
-        backgroundColor: orange,
-        width: paidValue <= 10 ? "10%" : paidValue + "%",
-        position: "absolute",
-        borderRadius: 10,
-        height: "100%",
-        justifyContent: "center"
+      backgroundColor: orange,
+      width: paidValue <= 10 ? '10%' : paidValue + '%',
+      position: 'absolute',
+      borderRadius: 10,
+      height: '100%',
+      justifyContent: 'center',
     },
-})
+  });
 
 export default function DebtPaidDetail({ amountPaid, totalAmount }: Props) {
-    const paidValue = useMemo(() => amountPaid && totalAmount ?
-        Number(((amountPaid / totalAmount) * 100).toFixed(2)) : 0
-        , [amountPaid, totalAmount])
+  const paidValue = useMemo(
+    () => (amountPaid && totalAmount ? Number(((amountPaid / totalAmount) * 100).toFixed(2)) : 0),
+    [amountPaid, totalAmount]
+  );
 
-    return (
-        <View style={styles.cardContainer}>
-            <View style={styles.cardLabel}>
-                <Text>Abonado</Text>
-                <Text
-                    style={{
-                        fontWeight: "bold",
-                        color: textBlack,
-                        maxWidth: "80%"
-                    }}
-                    numberOfLines={1}
-                >
-                    <Text
-                        style={{
-                            color: expense,
-                            maxWidth: "60%"
-                        }}
-                        numberOfLines={1}
-                    >
-                        ${amountPaid}
-                    </Text> / ${totalAmount}
-                </Text>
-            </View>
-            <View style={styles.progressBarBase}>
-                <View style={paidStyles(paidValue).progressBar}>
-                    <Text style={styles.processBarLabel}>
-                        {paidValue}%
-                    </Text>
-                </View>
-            </View>
+  return (
+    <View style={styles.cardContainer}>
+      <View style={styles.cardLabel}>
+        <Text
+          style={{
+            color: textBlack,
+            fontFamily: 'Gilroy-Bold',
+          }}
+        >
+          Abonado
+        </Text>
+        <Text
+          style={{
+            fontWeight: 'bold',
+            color: textBlack,
+            maxWidth: '80%',
+          }}
+          numberOfLines={1}
+        >
+          <Text
+            style={{
+              color: expense,
+              maxWidth: '60%',
+            }}
+            numberOfLines={1}
+          >
+            ${amountPaid}
+          </Text>{' '}
+          / ${totalAmount}
+        </Text>
+      </View>
+      <View style={styles.progressBarBase}>
+        <View style={paidStyles(paidValue).progressBar}>
+          <Text style={styles.processBarLabel}>{paidValue}%</Text>
         </View>
-    )
+      </View>
+    </View>
+  );
 }
