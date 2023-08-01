0561134c86d1cb5d487556eca452de0b47afad50
Billy Bautista <bbautista@litebox.ai>
2023-07-31 22:36:38 -0300

    installed react-native-localize to get locale of country

--- App.tsx
+++ App.tsx
@@ -1,47 +1,53 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { QueryClientProvider } from "react-query";
-import AuthProvider from "./src/v0.1/context/AuthContext";
-import GeneralProvider from "./src/v0.1/context/GeneralContext";
-import HomeTabs from "./src/v0.1/navigation/HomeTabs";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { QueryClientProvider } from 'react-query';
+import { getLocales } from 'react-native-localize';
+import AuthProvider from './src/v0.1/context/AuthContext';
+import GeneralProvider from './src/v0.1/context/GeneralContext';
+import HomeTabs from './src/v0.1/navigation/HomeTabs';
 
 // MORE STACK
-import DebtsModal from "./src/v0.1/components/common/Modals/DebtsModal";
-import UserDataScreen from "./src/v0.1/screens/MoreStack/UserData";
-import MoreScreen from "./src/v0.1/screens/MoreStack/MoreScreen";
+import DebtsModal from './src/v0.1/components/common/Modals/DebtsModal';
+import UserDataScreen from './src/v0.1/screens/MoreStack/UserData';
+import MoreScreen from './src/v0.1/screens/MoreStack/MoreScreen';
 
 // AUTH STACK
-import SignInScreen from "./src/v0.1/screens/AuthStack/SignInScreen";
-import SplashScreen from "./src/v0.1/screens/AuthStack/SplashScreen";
-import SignUpScreen from "./src/v0.1/screens/AuthStack/SignUpScreen";
+import SignInScreen from './src/v0.1/screens/AuthStack/SignInScreen';
+import SplashScreen from './src/v0.1/screens/AuthStack/SplashScreen';
+import SignUpScreen from './src/v0.1/screens/AuthStack/SignUpScreen';
 
 // BALANCE STACK
-import NewIncome from "./src/v0.1/screens/BalanceStack/NewIncome";
-import EditIncome from "./src/v0.1/screens/BalanceStack/EditIncome";
-import NewExpense from "./src/v0.1/screens/BalanceStack/NewExpense";
-import EditExpense from "./src/v0.1/screens/BalanceStack/EditExpense";
-import TransactionDetail from "./src/v0.1/screens/BalanceStack/TransactionDetail";
+import NewIncome from './src/v0.1/screens/BalanceStack/NewIncome';
+import EditIncome from './src/v0.1/screens/BalanceStack/EditIncome';
+import NewExpense from './src/v0.1/screens/BalanceStack/NewExpense';
+import EditExpense from './src/v0.1/screens/BalanceStack/EditExpense';
+import TransactionDetail from './src/v0.1/screens/BalanceStack/TransactionDetail';
 
 // CONTACT STACK
-import ClientsScreen from "./src/v0.1/screens/ContactStack/Clients";
-import ContactDetail from "./src/v0.1/screens/ContactStack/ContactDetail";
-import ProvidersScreen from "./src/v0.1/screens/ContactStack/Providers";
-import NewContact from "./src/v0.1/screens/ContactStack/NewContact";
+import ClientsScreen from './src/v0.1/screens/ContactStack/Clients';
+import ContactDetail from './src/v0.1/screens/ContactStack/ContactDetail';
+import ProvidersScreen from './src/v0.1/screens/ContactStack/Providers';
+import NewContact from './src/v0.1/screens/ContactStack/NewContact';
 
 // DEBT STACK
-import DebtsScreen from "./src/v0.1/screens/DebtStack/Debts";
-import DebtorScreen from "./src/v0.1/screens/DebtStack/DebtorProfile";
-import { queryClient } from "./src/v0.1/utils/queryClient";
-import { StatusBar } from "react-native";
-import DebtDetail from "./src/v0.1/screens/DebtStack/DebtDetail";
+import DebtsScreen from './src/v0.1/screens/DebtStack/Debts';
+import DebtorScreen from './src/v0.1/screens/DebtStack/DebtorProfile';
+import { queryClient } from './src/v0.1/utils/queryClient';
+import { StatusBar } from 'react-native';
+import DebtDetail from './src/v0.1/screens/DebtStack/DebtDetail';
 
-import customStyles from "./src/v0.1/styles/customStyles";
-import EditDebt from "./src/v0.1/screens/DebtStack/EditDebt";
+import customStyles from './src/v0.1/styles/customStyles';
+import EditDebt from './src/v0.1/screens/DebtStack/EditDebt';
 const { white } = customStyles;
-const statusBarStyle = "dark-content";
+const statusBarStyle = 'dark-content';
 
 const Stack = createNativeStackNavigator();
+
+const defaultLenguage = getLocales()[0].languageCode;
+const locale = getLocales();
+console.log('LENGUAJE ==>', defaultLenguage);
+console.log('LOCALE ==>', locale);
 
 const App = () => {
   return (
@@ -53,32 +59,29 @@ const App = () => {
             <Stack.Navigator
               screenOptions={{
                 headerShown: false,
-                animation: "none",
+                animation: 'none',
               }}
             >
-              <Stack.Screen name="Splash" component={SplashScreen} />
-              <Stack.Screen name="SignUp" component={SignUpScreen} />
-              <Stack.Screen name="Login" component={SignInScreen} />
-              <Stack.Screen name="HomeTabs" component={HomeTabs} />
-              <Stack.Screen name="NewIncome" component={NewIncome} />
-              <Stack.Screen name="EditIncome" component={EditIncome} />
-              <Stack.Screen name="NewExpense" component={NewExpense} />
-              <Stack.Screen name="EditExpense" component={EditExpense} />
-              <Stack.Screen name="DebtsModal" component={DebtsModal} />
-              <Stack.Screen
-                name="TransactionDetail"
-                component={TransactionDetail}
-              />
-              <Stack.Screen name="More" component={MoreScreen} />
-              <Stack.Screen name="UserData" component={UserDataScreen} />
-              <Stack.Screen name="Clients" component={ClientsScreen} />
-              <Stack.Screen name="Providers" component={ProvidersScreen} />
-              <Stack.Screen name="ContactDetail" component={ContactDetail} />
-              <Stack.Screen name="NewContact" component={NewContact} />
-              <Stack.Screen name="Debts" component={DebtsScreen} />
-              <Stack.Screen name="DebtDetail" component={DebtDetail}/>
-              <Stack.Screen name="EditDebt" component={EditDebt}/>
-              <Stack.Screen name="DebtorScreen" component={DebtorScreen} />
+              <Stack.Screen name='Splash' component={SplashScreen} />
+              <Stack.Screen name='SignUp' component={SignUpScreen} />
+              <Stack.Screen name='Login' component={SignInScreen} />
+              <Stack.Screen name='HomeTabs' component={HomeTabs} />
+              <Stack.Screen name='NewIncome' component={NewIncome} />
+              <Stack.Screen name='EditIncome' component={EditIncome} />
+              <Stack.Screen name='NewExpense' component={NewExpense} />
+              <Stack.Screen name='EditExpense' component={EditExpense} />
+              <Stack.Screen name='DebtsModal' component={DebtsModal} />
+              <Stack.Screen name='TransactionDetail' component={TransactionDetail} />
+              <Stack.Screen name='More' component={MoreScreen} />
+              <Stack.Screen name='UserData' component={UserDataScreen} />
+              <Stack.Screen name='Clients' component={ClientsScreen} />
+              <Stack.Screen name='Providers' component={ProvidersScreen} />
+              <Stack.Screen name='ContactDetail' component={ContactDetail} />
+              <Stack.Screen name='NewContact' component={NewContact} />
+              <Stack.Screen name='Debts' component={DebtsScreen} />
+              <Stack.Screen name='DebtDetail' component={DebtDetail} />
+              <Stack.Screen name='EditDebt' component={EditDebt} />
+              <Stack.Screen name='DebtorScreen' component={DebtorScreen} />
             </Stack.Navigator>
           </NavigationContainer>
         </GeneralProvider>
