a54c76bae3e0725b8982456253c594d3231d10ce
danilo2504 <danilobautista2004@gmail.com>
2023-09-22 15:48:38 +0200

    CU-86aybmxwd - [ BALANCE ] Fix query to get Last

--- src/v0.1/services/Transactions/useGetAllTransactions.ts
+++ src/v0.1/services/Transactions/useGetAllTransactions.ts
@@ -1,18 +1,25 @@
 import MauiApi from '../../clientProvider';
-import { getTransactionsResponseDto } from '../../../../../Maui-Backend/src/controllers/types';
+import {
+  getTransactionsQueryParamsDto,
+  getTransactionsResponseDto,
+} from '../../../../../Maui-Backend/src/controllers/types';
 import { QueryKey, useQuery, UseQueryOptions } from 'react-query';
 import { setHeaders } from '../../clientProvider/axiosConfig';
 
 const QUERY_NAME = 'Transactions';
 
-export const getTransactions = async (): Promise<getTransactionsResponseDto> => {
+export const getTransactions = async (
+  queryParams?: getTransactionsQueryParamsDto
+): Promise<getTransactionsResponseDto> => {
   await setHeaders();
-  const response = await MauiApi.get<getTransactionsResponseDto>('/getTransactions');
+  const response = await MauiApi.get<getTransactionsResponseDto>('/getTransactions', { params: queryParams });
 
   return response.data;
 };
 
-const useGetTransactions = (options?: UseQueryOptions<getTransactionsResponseDto>) =>
-  useQuery([QUERY_NAME] as QueryKey, () => getTransactions(), options);
+const useGetTransactions = (
+  queryParams?: getTransactionsQueryParamsDto,
+  options?: UseQueryOptions<getTransactionsResponseDto>
+) => useQuery([QUERY_NAME, queryParams] as QueryKey, () => getTransactions(queryParams), options);
 
 export default useGetTransactions;
