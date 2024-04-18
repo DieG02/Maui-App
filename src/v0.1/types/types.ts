61b3053bf4e59fea3972385479eac5192c447228
billybautista <billy.bautista.cornelio@gmail.com>
2024-04-18 00:16:14 -0300

    added types

---
+++ src/v0.1/types/types.ts
@@ -0,0 +1,267 @@
+// AUTH GOOGLE
+export type IAuthGoogleInput = {
+  email: string;
+  authSource: string;
+  name: string;
+  photo: string;
+};
+
+export type ICreateAuthGoogle = {
+  email: string;
+  name: string;
+  cellphone: string;
+  countryCode: string;
+  language: string;
+  country: string;
+  photo: string;
+};
+
+export type IAuthGoogle = {
+  id: string;
+  email: string;
+  token: string;
+  screenRedirect: string;
+};
+
+// Account
+export type IAccount = {
+  id: string;
+  name: string;
+  cellPhone: string;
+  country: string;
+  countryCode: string;
+  language: string;
+  image: string | null;
+  address: string | null;
+  ownerId: string;
+};
+
+export type IAccountInput = {
+  address: string;
+  cellPhone: string;
+  name: string;
+  countryCode: string;
+  image?: string;
+  country?: string;
+  language?: string;
+};
+
+// Contacts
+export type IContact = {
+  id: string;
+  name: string;
+  phone: string;
+  type: IContactType;
+  email: string;
+  note: string;
+  ownerId: string;
+};
+
+export enum IContactType {
+  CLIENT = 'CLIENT',
+  PROVIDER = 'PROVIDER',
+  EMPLOYEE = 'EMPLOYEE',
+}
+
+export type IContactInput = {
+  note: string;
+  email: string;
+  name: string;
+  phone: string;
+  type: IContactType;
+};
+
+// Balance
+
+export type IBalance = {
+  total_balance: number;
+  financialAccount: {
+    id: string;
+    businessId: string;
+    accountName: string;
+    currency: {
+      code: string;
+      locale: string;
+      isoCode: string;
+    };
+  };
+};
+
+export type IBalanceItem = {
+  month: string;
+  year: string;
+  incomes: number;
+  expenses: number;
+  total: number;
+  previousBalance: number;
+};
+
+export type IMonthlyBalance = {
+  currency: {
+    code: string;
+    locale: string;
+  };
+  balance: IBalanceItem[];
+};
+
+export type IMontlyStats = {
+  incomes: string;
+  expenses: string;
+  debt: string;
+  toCollect: string;
+};
+
+// Countries
+export type ICountry = {
+  id: string;
+  name: string;
+  isoCode: string;
+};
+
+// CountryCode
+
+export type ICountryCode = {
+  id: string;
+  name: string;
+  isoCode: string;
+  prefix: string;
+};
+
+// Debt
+export type IDebtDetail = {
+  contactId: string;
+  contactName: string;
+  initialDate: string;
+  totalToPay: number;
+  amountDebts: number;
+  financialAccount: {
+    id: string;
+    accountName: string;
+    currency: {
+      code: string;
+      locale: string;
+      isoCode: string;
+    };
+  };
+};
+
+export type IDebts = {
+  incomes: IDebtDetail[];
+  expenses: IDebtDetail[];
+};
+
+export enum TransactionType {
+  DEBIT = 'DEBIT',
+  CREDIT = 'CREDIT',
+}
+
+export type IDebt = {
+  id: string;
+  initial_date: Date | null;
+  total_amount: number;
+  type: TransactionType;
+  createdAt: Date;
+  debtorId: string | null;
+  financialAccountId: string | null;
+  ownerId: string;
+};
+
+export type IDebtor = {
+  financialAccount: {
+    id: string;
+    accountName: string;
+    currency: {
+      code: string;
+      locale: string;
+      isoCode: string;
+    };
+  };
+  profile: IContact;
+  status: {
+    totalDebt: number;
+    totalToPay: number;
+  };
+  debts: IDebt[];
+  payments: ITransaction[];
+};
+
+export type IPayDebtInput = {
+  type: TransactionType;
+  paymentAmount: number;
+  paidAt: string;
+  payment_method: IPaymentMethod;
+  description: string;
+};
+
+export enum IPaymentMethod {
+  CASH = 'CASH',
+  CARD = 'CARD',
+  BANK_TRANSFER = 'BANK_TRANSFER',
+  OTHER = 'OTHER',
+  ONLINE = 'ONLINE',
+  NONE = 'NONE',
+}
+
+export enum TransactionStatus {
+  DEBT = 'DEBT',
+  APPROVED = 'APPROVED',
+  PAID = 'PAID',
+}
+
+export type ITransaction = {
+  status: TransactionStatus;
+  type: TransactionType;
+  payment_method: IPaymentMethod;
+  id: string;
+  total_amount: number;
+  description: string;
+  date: string;
+  categoryId: string;
+  contactId: string;
+  debtId: string;
+  financialAccountId: string;
+  ownerId: string;
+};
+
+export interface ITransactionDetail extends ITransaction {
+  category: ITransactionCategory;
+  contact: IContact;
+  financialAccount: {
+    accountName: string;
+    currency: {
+      code: string;
+      locale: string;
+    };
+  };
+}
+
+export type IQueryTransaction = {
+  take?: number;
+  type?: TransactionType;
+  status?: TransactionStatus;
+};
+
+export type ITransactionInput = {
+  status: TransactionStatus;
+  type: TransactionType;
+  total_amount: number;
+  description: string;
+  date: string;
+  payment_method: IPaymentMethod;
+  categoryId?: string;
+  contactId: string;
+};
+
+export enum TransactionGroup {
+  TRANSACTION = 'TRANSACTION',
+  PAYMENT = 'PAYMENT',
+}
+
+export type ITransactionCategory = {
+  id: string;
+  name: string;
+  code: string;
+  image: string;
+  type: TransactionType;
+  group: TransactionGroup;
+};
