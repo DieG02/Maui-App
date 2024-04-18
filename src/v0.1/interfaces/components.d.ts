fcca82c325eae6005a53719ba9b8f4bb0e6079f7
billybautista <billy.bautista.cornelio@gmail.com>
2024-04-18 00:01:40 -0300

    clean components type

--- src/v0.1/interfaces/components.d.ts
+++ src/v0.1/interfaces/components.d.ts
@@ -1,33 +1,3 @@
-interface ITransaction {
-  name: string;
-  value: string;
-  color: string;
-  icon: string;
-  type: string;
-  state: string;
-  id: string;
-  paymentMethod: string;
-  categoryId: string;
-  category: {
-    id: string;
-    name: string;
-    imageUrl: string;
-  };
-}
-
-interface IProduct {
-  image: string;
-  price: string;
-  description: string;
-  stock: number;
-}
-
-interface IContact {
-  id: string;
-  phone: string;
-  name: string;
-}
-
 interface IDebtContact {
   id: string;
   name: string;
