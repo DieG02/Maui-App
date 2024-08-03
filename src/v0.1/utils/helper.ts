69e3940fac5a7397ffe6d1df5fe8b52df8957efc
billybautista <billy.bautista.cornelio@gmail.com>
2024-08-03 17:14:51 -0300

    fix eslint of utils

--- src/v0.1/utils/helper.ts
+++ src/v0.1/utils/helper.ts
@@ -1,25 +1,23 @@
 export const getInitialLetters = (name: string) => {
   const separator = (fullName: string) => {
-    const letters = fullName.split(" ").map((e) => e.slice(0, 1).toUpperCase());
+    const letters = fullName.split(' ').map(e => e.slice(0, 1).toUpperCase());
     return letters.slice(0, 3);
   };
 
-  const letters = name.includes(" ")
-    ? separator(name)
-    : name.slice(0, 1).toUpperCase();
+  const letters = name.includes(' ') ? separator(name) : name.slice(0, 1).toUpperCase();
   return letters;
 };
 
-export const parseYYMMDD = (ISODate: string) => ISODate.split('T')[0]
+export const parseYYMMDD = (ISODate: string) => ISODate.split('T')[0];
 
 export const parseDDMMYY = (ISODate: string) => {
   const date = parseYYMMDD(ISODate).split('-');
 
-  return `${date[2]}-${date[1]}-${date[0]}`
-}
+  return `${date[2]}-${date[1]}-${date[0]}`;
+};
 
 export const parseDDMM = (ISODate: string) => {
   const date = parseYYMMDD(ISODate).split('-');
 
-  return `${date[2]}-${date[1]}`
-}
+  return `${date[2]}-${date[1]}`;
+};
--- src/v0.1/utils/toast.ts
+++ src/v0.1/utils/toast.ts
@@ -1,9 +1,5 @@
-import { ToastAndroid } from 'react-native'
+import { ToastAndroid } from 'react-native';
 
 export const showToast = (message: string) => {
-    ToastAndroid.showWithGravity(
-        message,
-        ToastAndroid.LONG,
-        ToastAndroid.TOP
-    )
-}
+  ToastAndroid.showWithGravity(message, ToastAndroid.LONG, ToastAndroid.TOP);
+};
